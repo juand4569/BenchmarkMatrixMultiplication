5e2bf0492e9ccb835dd57632b9526d6ca44d2fcf
juand4569 <juand24601@gmail.com>
2025-10-23 20:48:52 +0200

    feat(matrix-class): add a matrix class to ease the creation of the matrix multiplication

--- python/src/main/matrix.py
+++ python/src/main/matrix.py
@@ -1,21 +1,27 @@
-import random
-from time import *
-
-n = 1024
-
-A = [[random.random() for _ in range(n)] for _ in range(n)]
-B = [[random.random() for _ in range(n)] for _ in range(n)]
-C = [[0 for _ in range(n)] for _ in range(n)]
-
-start = time()
-
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            C[i][j] += A[i][k] * B[k][j]
-
-end = time()
-
-print("%.6f" % (end-start))
+from random import Random
 
 
+class MatrixMultiplier:
+    def __init__(self, size, random_seed=None):
+        self.size = size
+        self.data = []
+        self._rng = Random(random_seed)
+
+    def fill_random(self):
+        self.data = [[self._rng.random() for _ in range(self.size)]
+                     for _ in range(self.size)]
+        return self
+
+    def multiply_with(self, other):
+        if self.size != other.size:
+            raise ValueError("Matrix dimensions must match")
+
+        result = MatrixMultiplier(self.size)
+        result.data = [[0.0] * self.size for _ in range(self.size)]
+
+        for i in range(self.size):
+            for j in range(self.size):
+                for k in range(self.size):
+                    result.data[i][j] += self.data[i][k] * other.data[k][j]
+
+        return result
